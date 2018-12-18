c4d0b8f104284e2eeeba7dfee1625a3be49049d5
Neil Ricci <iccir919@gmail.com>
2018-12-18 10:34:28 -0800

    Change the name of function from the example to my own

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function tiny(string) {
+module.exports = function first(string) {
   if (typeof string !== "string") throw new TypeError("First wants a string!");
   return string.replace(/\s/g, "");
 };
